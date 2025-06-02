e0477c2a49b019947dc6f305dc501e6dd5280926
Asamaurdhava <eruditevsr@gmail.com>
2025-06-02 18:34:47 +0530

    Initial Commit

---
+++ RockPaperScissorsTests/RockPaperScissorsTests.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsTests.swift
+//  RockPaperScissorsTests
+//
+//  Created by Vishesh S Rajput on 02/06/25.
+//
+
+import Testing
+@testable import RockPaperScissors
+
+struct RockPaperScissorsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
